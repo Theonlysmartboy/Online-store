51f80050e6cab833bd986a881049dba7aadf0d7d
Tosby <o2jose43@gmail.com>
2017-03-08 17:52:58 +0300

    added code to display the duration for the page to render

--- src/main/java/com/otema/onlinestore/controller/HomeController.java
+++ src/main/java/com/otema/onlinestore/controller/HomeController.java
@@ -3,7 +3,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Date;
-import java.util.TimeZone;
 /**
  *
  * @author Maseno
